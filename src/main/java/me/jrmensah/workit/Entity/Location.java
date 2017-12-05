dbd0e0538444f22bce02ea5244ce7cbf1041eb4a
jrmensah <jerilynmensah@gmail.com>
2017-12-05 16:44:58 -0500

    Jerilyn- location list shows, but does not store entries

--- src/main/java/me/jrmensah/workit/Entity/Location.java
+++ src/main/java/me/jrmensah/workit/Entity/Location.java
@@ -1,11 +1,10 @@
 package me.jrmensah.workit.Entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Location
@@ -13,16 +12,26 @@ public class Location
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    ////
+//    @NotNull
+//    @Size(min = 3)
+    private String description;
 
-    @NotNull
-    @Size(min=3)
-    private String location;
+    @ManyToMany(mappedBy = "locations")
+    private Set<Trainer> training;
 
-    public Location() {
+    @ManyToMany(mappedBy = "locations")
+    private Set<Client> clients;
+
+    public Location()
+    {
     }
 
-    public Location(String location) {
-        this.location = location;
+    public Location(String description)
+    {
+        this.description = description;
+        this.training=new HashSet<Trainer>();
+        this.clients=new HashSet<Client>();
     }
 
     public long getId() {
@@ -33,11 +42,27 @@ public class Location
         this.id = id;
     }
 
-    public String getLocation() {
-        return location;
+    public String getDescription() {
+        return description;
     }
 
-    public void setLocation(String location) {
-        this.location = location;
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Trainer> getTraining() {
+        return training;
+    }
+
+    public void setTraining(Set<Trainer> training) {
+        this.training = training;
+    }
+
+    public Set<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(Set<Client> clients) {
+        this.clients = clients;
     }
 }
